3b1b499b8e88fd26ee691b21a933163ef6d641b0
Golos Ilya <ilyagolos8@gmail.com>
2023-11-11 12:49:45 +0300

    add completed lesson 5

--- module05/src/main/java/ru/sberbank/edu/App.java
+++ module05/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,26 @@
 package ru.sberbank.edu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        GeoPosition omskPosition = new GeoPosition(54, 59, 47, 73, 22, 20);
+        GeoPosition moscowPosition = new GeoPosition(55, 45, 21, 37, 37, 1);
+        CityInfo moscow = new CityInfo("Moscow", moscowPosition);
+        CityInfo omsk = new CityInfo("Omsk", omskPosition);
+        CityInfo doubleMoscow = new CityInfo("Moscow", moscowPosition);
+        TravelService service = new TravelService();
+
+        service.add(moscow);
+        service.add(omsk);
+        System.out.println(omsk.getPosition());
+        System.out.println(moscow.getPosition());
+
+        System.out.println(service.getDistance("Moscow","Omsk"));
+
     }
 }
--- module05/src/main/java/ru/sberbank/edu/GeoPosition.java
+++ module05/src/main/java/ru/sberbank/edu/GeoPosition.java
@@ -22,9 +22,30 @@ public class GeoPosition {
      * @param longitudeGradus - longitude in gradus
      *                        Possible values: 55, 55(45'07''), 59(57'00'')
      */
-    public GeoPosition(String latitudeGradus, String longitudeGradus) {
-        // parse and set latitude and longitude in radian
+    public GeoPosition(int degreeLat,int minutesLat, int secondsLat,int degreeLong,int minutesLong,int secondsLong) {
+        this.latitude = degreeLat+ (double) minutesLat/60+ (double) secondsLat /3600;
+        this.longitude = degreeLong+  (double) minutesLong/60+ (double) secondsLong /3600;
+        this.latitude = Math.toRadians(this.latitude);
+        this.longitude = Math.toRadians(this.longitude);
     }
 
     // getters and toString
+
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "GeoPosition{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+
 }
