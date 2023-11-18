f773692b2f89ec2103611026915946daf43d8c67
Golos Ilya <ilyagolos8@gmail.com>
2023-11-18 12:33:40 +0300

    add completed lesson 6 without tests

--- module06/src/main/java/ru/sberbank/edu/repository/CarRepository.java
+++ module06/src/main/java/ru/sberbank/edu/repository/CarRepository.java
@@ -2,7 +2,6 @@ package ru.sberbank.edu.repository;
 
 import ru.sberbank.edu.model.Car;
 
-import java.sql.SQLException;
 import java.util.Set;
 
 public interface CarRepository extends Repository<Car, String> {
