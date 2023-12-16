10deac2cb38dfa3f9bd356a154e88b607b36db4e
Golos Ilya <ilyagolos8@gmail.com>
2023-12-16 13:14:57 +0300

    add completed lesson 10

---
+++ module10/src/main/java/ru/sberbank/edu/Controller.java
@@ -0,0 +1,17 @@
+package ru.sberbank.edu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
+@RequestMapping(value = "/get")
+public class Controller {
+
+    @GetMapping
+    public ModelAndView info(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("/src/main/webapp/WEB-INF/index.html");
+        return modelAndView;
+    }
+}
